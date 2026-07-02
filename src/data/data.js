38f724513bc2f7e17fc9e7aa13c96b2f42a440e6
shayanmc <shayanmc.dev@gmail.com>
2026-07-02 11:34:26 +0330

    add data

---
+++ src/data/data.js
@@ -0,0 +1,94 @@
+const ArticleData = [
+  {
+    id: 1,
+    title: "sample1",
+    content: `
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus reiciendis quo ipsam unde ullam, eos quidem? Eos repudiandae exercitationem a facilis, eveniet doloribus atque laborum.
+
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, doloribus id laborum deleniti eos neque, ea tempora nesciunt, perferendis officia eligendi. Dicta eum mollitia id porro culpa laudantium magnam voluptate, corporis in explicabo, quaerat quidem.
+
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus reiciendis quo ipsam unde ullam, eos quidem? Eos repudiandae exercitationem a facilis, eveniet doloribus atque laborum.
+
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, doloribus id laborum deleniti eos neque, ea tempora nesciunt, perferendis officia eligendi. Dicta eum mollitia id porro culpa laudantium magnam voluptate, corporis in explicabo, quaerat quidem.
+      `,
+    date: "01/02/2003",
+    author: "moein",
+  },
+  {
+    id: 2,
+    title: "sample2",
+    content: `
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus reiciendis quo ipsam unde ullam, eos quidem? Eos repudiandae exercitationem a facilis, eveniet doloribus atque laborum.
+
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, doloribus id laborum deleniti eos neque, ea tempora nesciunt, perferendis officia eligendi. Dicta eum mollitia id porro culpa laudantium magnam voluptate, corporis in explicabo, quaerat quidem.
+
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus reiciendis quo ipsam unde ullam, eos quidem? Eos repudiandae exercitationem a facilis, eveniet doloribus atque laborum.
+
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, doloribus id laborum deleniti eos neque, ea tempora nesciunt, perferendis officia eligendi. Dicta eum mollitia id porro culpa laudantium magnam voluptate, corporis in explicabo, quaerat quidem.
+      `,
+    date: "01/02/2003",
+    author: "farhad",
+  },
+  {
+    id: 3,
+    title: "sample3",
+    content: `
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus reiciendis quo ipsam unde ullam, eos quidem? Eos repudiandae exercitationem a facilis, eveniet doloribus atque laborum.
+
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, doloribus id laborum deleniti eos neque, ea tempora nesciunt, perferendis officia eligendi. Dicta eum mollitia id porro culpa laudantium magnam voluptate, corporis in explicabo, quaerat quidem.
+
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus reiciendis quo ipsam unde ullam, eos quidem? Eos repudiandae exercitationem a facilis, eveniet doloribus atque laborum.
+
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, doloribus id laborum deleniti eos neque, ea tempora nesciunt, perferendis officia eligendi. Dicta eum mollitia id porro culpa laudantium magnam voluptate, corporis in explicabo, quaerat quidem.
+      `,
+    date: "01/02/2003",
+    author: "asd",
+  },
+  {
+    id: 4,
+    title: "sample4",
+    content: `
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus reiciendis quo ipsam unde ullam, eos quidem? Eos repudiandae exercitationem a facilis, eveniet doloribus atque laborum.
+
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, doloribus id laborum deleniti eos neque, ea tempora nesciunt, perferendis officia eligendi. Dicta eum mollitia id porro culpa laudantium magnam voluptate, corporis in explicabo, quaerat quidem.
+
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus reiciendis quo ipsam unde ullam, eos quidem? Eos repudiandae exercitationem a facilis, eveniet doloribus atque laborum.
+
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, doloribus id laborum deleniti eos neque, ea tempora nesciunt, perferendis officia eligendi. Dicta eum mollitia id porro culpa laudantium magnam voluptate, corporis in explicabo, quaerat quidem.
+      `,
+    date: "01/02/2003",
+    author: "reza",
+  },
+  {
+    id: 5,
+    title: "sample5",
+    content: `
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus reiciendis quo ipsam unde ullam, eos quidem? Eos repudiandae exercitationem a facilis, eveniet doloribus atque laborum.
+
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, doloribus id laborum deleniti eos neque, ea tempora nesciunt, perferendis officia eligendi. Dicta eum mollitia id porro culpa laudantium magnam voluptate, corporis in explicabo, quaerat quidem.
+
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus reiciendis quo ipsam unde ullam, eos quidem? Eos repudiandae exercitationem a facilis, eveniet doloribus atque laborum.
+
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, doloribus id laborum deleniti eos neque, ea tempora nesciunt, perferendis officia eligendi. Dicta eum mollitia id porro culpa laudantium magnam voluptate, corporis in explicabo, quaerat quidem.
+      `,
+    date: "01/02/2003",
+    author: "alii",
+  },
+  {
+    id: 6,
+    title: "sample6",
+    content: `
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus reiciendis quo ipsam unde ullam, eos quidem? Eos repudiandae exercitationem a facilis, eveniet doloribus atque laborum.
+
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, doloribus id laborum deleniti eos neque, ea tempora nesciunt, perferendis officia eligendi. Dicta eum mollitia id porro culpa laudantium magnam voluptate, corporis in explicabo, quaerat quidem.
+
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus reiciendis quo ipsam unde ullam, eos quidem? Eos repudiandae exercitationem a facilis, eveniet doloribus atque laborum.
+
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, doloribus id laborum deleniti eos neque, ea tempora nesciunt, perferendis officia eligendi. Dicta eum mollitia id porro culpa laudantium magnam voluptate, corporis in explicabo, quaerat quidem.
+      `,
+    date: "01/02/2003",
+    author: "shayan",
+  },
+];
+
+export default ArticleData
